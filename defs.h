3fcb75461ae16962ed1e83b60c1b061e831d70e1
Aditya Gupta <adityag@linux.ibm.com>
2023-12-04 16:16:19 +0530

    fix 'info threads' command
    Currently the output is like this ppc64le:
    
    crash> info threads
      Id   Target Id         Frame
      1    CPU 0             <unavailable> in ?? ()
      2    CPU 1             0xc000000000025d00 in ppc_panic_event (this=<optimized out>, event=<optimized out>, ptr=0x0) at arch/powerpc/kernel/setup-common.c:712
      3    CPU 2             0xc000000000025d00 in ppc_panic_event (this=<optimized out>, event=<optimized out>, ptr=0x0) at arch/powerpc/kernel/setup-common.c:712
      4    CPU 3             0xc000000000025d00 in ppc_panic_event (this=<optimized out>, event=<optimized out>, ptr=0x0) at arch/powerpc/kernel/setup-common.c:712
    * 5    CPU 4             0xc000000000025d00 in ppc_panic_event (this=<optimized out>, event=<optimized out>, ptr=0x0) at arch/powerpc/kernel/setup-common.c:712
      6    CPU 5             0xc000000000025d00 in ppc_panic_event (this=<optimized out>, event=<optimized out>, ptr=0x0) at arch/powerpc/kernel/setup-common.c:712
      7    CPU 6             0xc000000000025d00 in ppc_panic_event (this=<optimized out>, event=<optimized out>, ptr=0x0) at arch/powerpc/kernel/setup-common.c:712
      8    CPU 7             0xc000000000025d00 in ppc_panic_event (this=<optimized out>, event=<optimized out>, ptr=0x0) at arch/powerpc/kernel/setup-common.c:712
    
    There are two issues here:
    
    1. CPU 0 always shows '<unavailable>' frame:
    
       This is due to 'thread 0' being initialised in 'crash_target_init',
       but it's very early in crash init, so it could not get any registers
       from the vmcore.
    
       And, since GDB caches registers (whether it is there or now), it
       keeps thinking that registers for this thread are unavailable, so all
       future usage of thread 0 also shows unavailable registers & frame
    
       Fix this by refreshing the register cache of all threads in
       'gdb_refresh_regcache', at task_init
    
    2. All other CPUs show the same frame:
    
       For each thread, GDB depends on crash to give it registers, ie.
       crash_target::fetch_registers, but this in turn depends on crash's
       current context
    
       GDB internally switches it's thread context internally to each thread
       one by one, while printing the thread's frame, similarly switch
       crash's context when the thread context is changed, so that it
       matches the thread context in gdb (such as, CPU 3 in crash for thread
       2(CPU 3) in gdb).
    
       Also, since we are switching context multiple times, add an argument
       to 'set_cpu' to not print the context everytime
    
    With these fixed, it will show all threads, as well as correct registers
    and frame. It might still show similar frame for many threads, due to
    all of them being on same NMI exception crash path, but if checked with
    'info registers', they will have different registers.
    
    Signed-off-by: Aditya Gupta <adityag@linux.ibm.com>

--- defs.h
+++ defs.h
@@ -5940,7 +5940,7 @@ extern char *help_map[];
  *  task.c
  */ 
 void task_init(void);
-int set_context(ulong, ulong);
+int set_context(ulong, ulong, uint);
 void show_context(struct task_context *);
 ulong pid_to_task(ulong);
 ulong task_to_pid(ulong);
@@ -6044,7 +6044,7 @@ void parse_kernel_version(char *);
 #define SHOW_LOG_AUDIT (0x8)
 #define SHOW_LOG_CTIME (0x10)
 #define SHOW_LOG_SAFE  (0x20)
-void set_cpu(int);
+void set_cpu(int cpu, int print_context);
 void clear_machdep_cache(void);
 struct stack_hook *gather_text_list(struct bt_info *);
 int get_cpus_online(void);
@@ -7978,5 +7978,6 @@ enum ppc64_regnum {
 
 /* crash_target.c */
 extern int gdb_change_cpu_context (unsigned int cpu);
+extern void gdb_refresh_regcache (unsigned int cpu);
 
 #endif /* !GDB_COMMON */
